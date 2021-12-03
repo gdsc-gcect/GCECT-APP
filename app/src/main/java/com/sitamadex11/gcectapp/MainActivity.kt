e2748ef8d1ce5278919c8364f2ac83b16418485e
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-12-03 13:05:49 +0530

    Lets Begin

---
+++ app/src/main/java/com/sitamadex11/gcectapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sitamadex11.gcectapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
