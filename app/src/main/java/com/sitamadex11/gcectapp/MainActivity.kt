dcb63ac7f011ef9861ff2deb4a3b89aaa314936b
Chayan Das <50614321+chayan-dev@users.noreply.github.com>
2021-12-15 21:08:19 +0530

    nav drawer and action bar added-updated

--- app/src/main/java/com/sitamadex11/gcectapp/MainActivity.kt
+++ app/src/main/java/com/sitamadex11/gcectapp/MainActivity.kt
@@ -12,10 +12,20 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
+import android.widget.ExpandableListView
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+
+    var listAdapter: ExpandableListAdapter? = null
+    var expListView: ExpandableListView? = null
+    var listDataHeader: List<String>? = null
+    var listDataChild: HashMap<String, List<String>>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,25 +44,45 @@ class MainActivity : AppCompatActivity() {
 
         val navView=findViewById<View>(R.id.navView) as NavigationView
 
-        navView.setNavigationItemSelectedListener{
-            when(it.itemId){
-                R.id.nav_home -> {Toast.makeText(applicationContext,
-                    "Clicked Home",Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                }
+//        navView.setNavigationItemSelectedListener{
+//            when(it.itemId){
+//                R.id.nav_home -> {Toast.makeText(applicationContext,
+//                    "Clicked Home",Toast.LENGTH_SHORT).show()
+//                    drawerLayout.closeDrawer(GravityCompat.START)
+//                }
+//
+//                R.id.nav_aboutus ->{ Toast.makeText(applicationContext,
+//                    "Clicked About Us",Toast.LENGTH_SHORT).show()
+//                    drawerLayout.closeDrawer(GravityCompat.START)
+//                }
+//                R.id.nav_acad -> {Toast.makeText(applicationContext,
+//                    "Clicked Academics",Toast.LENGTH_SHORT).show()
+//                    drawerLayout.closeDrawer(GravityCompat.START)
+//                }
+//
+//            }
+//            true
+//        }
 
-                R.id.nav_aboutus ->{ Toast.makeText(applicationContext,
-                    "Clicked About Us",Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                }
-                R.id.nav_acad -> {Toast.makeText(applicationContext,
-                    "Clicked Academics",Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                }
+        // get the listview
+        // get the listview
+        var expListView = findViewById<View>(R.id.lvExp) as ExpandableListView
 
-            }
-            true
-        }
+        // preparing list data
+
+        // preparing list data
+        prepareListData()
+
+        val listAdapter = listDataHeader?.let { listDataChild?.let { it1 ->
+            ExpandableListAdapter(this, it,
+                it1
+            )
+        } }
+
+        // setting list adapter
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter)
 
     }
 
@@ -63,4 +93,51 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+    /*
+     * Preparing the list data
+     */
+    private fun prepareListData() {
+        listDataHeader = ArrayList<String>()
+        listDataChild = HashMap<String, List<String>>()
+
+        // Adding child data
+        (listDataHeader as ArrayList<String>).add("Home")
+        (listDataHeader as ArrayList<String>).add("About Us")
+        (listDataHeader as ArrayList<String>).add("Academics")
+        (listDataHeader as ArrayList<String>).add("Infrastructure")
+
+        // Adding child data
+        val home: MutableList<String> = ArrayList()
+//        top250.add("The Shawshank Redemption")
+//        top250.add("The Godfather")
+//        top250.add("The Godfather: Part II")
+//        top250.add("Pulp Fiction")
+//        top250.add("The Good, the Bad and the Ugly")
+//        top250.add("The Dark Knight")
+//        top250.add("12 Angry Men")
+        val aboutus: MutableList<String> = ArrayList()
+//        nowShowing.add("The Conjuring")
+//        nowShowing.add("Despicable Me 2")
+//        nowShowing.add("Turbo")
+//        nowShowing.add("Grown Ups 2")
+//        nowShowing.add("Red 2")
+//        nowShowing.add("The Wolverine")
+        val acad: MutableList<String> = ArrayList()
+        acad.add("Departments")
+        acad.add("Syllabus")
+        acad.add("Research and development")
+        acad.add("Collaboration")
+        acad.add("Consultancy")
+        acad.add("Academic Regulation")
+        acad.add("Academic Calender")
+        acad.add("Mandatory Disclosures")
+        val infra: MutableList<String> = ArrayList()
+        listDataChild!![(listDataHeader as ArrayList<String>)[0]] = home // Header, Child data
+        listDataChild!![(listDataHeader as ArrayList<String>)[1]] = aboutus
+        listDataChild!![(listDataHeader as ArrayList<String>)[2]] = acad
+        listDataChild!![(listDataHeader as ArrayList<String>)[3]] = infra
+
+    }
+
 }
