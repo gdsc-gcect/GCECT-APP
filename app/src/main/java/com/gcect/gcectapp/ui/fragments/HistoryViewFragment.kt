c454bd68dd39260bbe8fc8762c31f7614accec3c
SHOUNAK DAS <shounakdas30@gmail.com>
2022-02-23 11:38:06 +0530

    ui implementation

--- app/src/main/java/com/gcect/gcectapp/ui/fragments/HistoryViewFragment.kt
+++ app/src/main/java/com/gcect/gcectapp/ui/fragments/HistoryViewFragment.kt
@@ -1,35 +1,54 @@
 package com.gcect.gcectapp.ui.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-import com.facebook.shimmer.ShimmerFrameLayout
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.gcect.gcectapp.R
-import com.gcect.gcectapp.databinding.StaticAboutUsFragmentBinding
+import com.gcect.gcectapp.databinding.HistoryViewFragmentBinding
 
 class HistoryViewFragment: Fragment() {
-    private lateinit var binding: StaticAboutUsFragmentBinding
+    private lateinit var binding: com.gcect.gcectapp.databinding.HistoryViewFragmentBinding
     private val historyImgUrl = "https://firebasestorage.googleapis.com/v0/b/gcect-app.appspot.com/o/About%20Us%2FHistory%2Fgcect_history_img.jpg?alt=media&token=e5e67b13-5254-4fa4-aebd-9e7b6d0c1f57"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.static_about_us_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.history_view_fragment, container, false)
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.pageDesc = getString(R.string.history_page)
-        binding.pageTitle = "HISTORY"
+        binding.pageDesc.text = getString(R.string.history_page)
+        binding.txtPageTitle.text = "HISTORY"
+        val progressbar: ProgressBar = binding.imgProgressBar
         val historyImg = binding.gcectAboutUsImage
-        Glide.with(requireContext()).load(historyImgUrl).into(historyImg)
+        Glide.with(requireContext()).load(historyImgUrl)
+            .listener(object : RequestListener<Drawable>{
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    progressbar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Failed to load image...", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    progressbar.visibility = View.GONE
+                    return false
+                }
+
+            })
+            .into(historyImg)
         onBackPressed()
     }
     private fun onBackPressed() {
--- app/src/main/java/com/gcect/gcectapp/ui/fragments/TheInstituteViewFragment.kt
+++ app/src/main/java/com/gcect/gcectapp/ui/fragments/TheInstituteViewFragment.kt
@@ -1,36 +1,54 @@
 package com.gcect.gcectapp.ui.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.gcect.gcectapp.R
-import com.gcect.gcectapp.databinding.StaticAboutUsFragmentBinding
+import com.gcect.gcectapp.databinding.TheInstituteViewFragmentBinding
 
 class TheInstituteViewFragment: Fragment() {
-    private lateinit var binding: StaticAboutUsFragmentBinding
+    private lateinit var binding: TheInstituteViewFragmentBinding
     private val instituteImgUrl = "https://firebasestorage.googleapis.com/v0/b/gcect-app.appspot.com/o/About%20Us%2FThe%20Institute%2Fimg%2001.jpg?alt=media&token=9482f052-a36a-4828-ac34-7a0a77e7ff3f"
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.static_about_us_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.the_institute_view_fragment, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.pageTitle = "About Us"
-        binding.pageDesc = getString(R.string.the_institute)
+        binding.txtPageTitle.text = "The Institute"
+        val progressbar: ProgressBar = binding.imgProgressBar
         val instituteImg = binding.gcectAboutUsImage
-        Glide.with(requireContext()).load(instituteImgUrl).into(instituteImg)
+        Glide.with(requireContext()).load(instituteImgUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    progressbar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Failed to load image...", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    progressbar.visibility = View.GONE
+                    return false
+                }
+
+            })
+            .into(instituteImg)
         onBackPressed()
     }
     private fun onBackPressed() {
