6c0442a4faf2f43c852f76ea73d8867b8d1e4adf
Souvik Dey <deysouvik700@gmail.com>
2022-03-08 18:53:47 +0530

    only textView

--- app/src/main/java/com/gcect/gcectapp/model/ConsultancyPageProjectDetail.kt
+++
@@ -1,5 +0,0 @@
-package com.gcect.gcectapp.model
-
-data class ConsultancyDetails(
-    val consultancytext:String
-)
--- app/src/main/java/com/gcect/gcectapp/ui/fragments/Consultancy_Fragment.kt
+++ app/src/main/java/com/gcect/gcectapp/ui/fragments/Consultancy_Fragment.kt
@@ -6,18 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.gcect.gcectapp.R
-import com.gcect.gcectapp.adapters.ConsultancyRecyclerAdapter
+
 import com.gcect.gcectapp.databinding.FragmentConsultancyBinding
-import com.gcect.gcectapp.viewmodels.ConsultancyViewmodel
+
 
 
 class Consultancy_Fragment : Fragment() {
     lateinit var binding: FragmentConsultancyBinding
-    private val consultancyViewmodel: ConsultancyViewmodel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,17 +28,11 @@ class Consultancy_Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        CollaborationrecyclerViewSetup(binding.rvConsultancy)
+
 
     }
 
 
-    private fun CollaborationrecyclerViewSetup(recyclerView: RecyclerView){
-        // Setting the layout manager
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        // This will pass the ArrayList to our Adapter
-        val adapter = ConsultancyRecyclerAdapter(requireContext(),consultancyViewmodel.listOfConsultancyDetail)
-        recyclerView.adapter = adapter
-    }
+
 
 }
