a51a0f91e92887b2e30e1863804a85a56ac7df1d
Debashish Kundu <debashish.joy@gmail.com>
2022-04-06 05:01:24 +0530

    Fully implemented transparent status bar

--- app/src/main/java/com/gcect/gcectapp/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/gcect/gcectapp/ui/activity/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import com.gcect.gcectapp.R
 
@@ -11,13 +12,13 @@ class SplashScreenActivity : AppCompatActivity() {
     private val SPLASH_SCREEN_TIME_OUT = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed(Runnable {
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
